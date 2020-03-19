bf27ee9185105d50286425c31b9c8ba7a586fd76
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-03-19 22:50:16 +0200

    Initial Commit

---
+++ CornerRadius/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CornerRadius
+//
+//  Created by ramil on 19.03.2020.
+//  Copyright © 2020 com.ri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
